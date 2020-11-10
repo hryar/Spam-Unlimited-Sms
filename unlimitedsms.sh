0706a2b19975c135afdd5e07a7f85bb226ff4e3c
hryar <74238336+hryar@users.noreply.github.com>
2020-11-10 22:02:08 +0700

    Create unlimitedsms.sh

---
+++ unlimitedsms.sh
@@ -0,0 +1,165 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "unlimitedsms"
+sleep 1
+echo "\033[33;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[36;1m★""\033[34;1mGumay Talang""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Spam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mBukalapak"
+echo "\033[35;1m2.""\033[36;1mTelkomsel"
+echo "\033[35;1m3.""\033[36;1mGrab"
+echo "\033[35;1m4.""\033[36;1mTokopedia"
+echo "\033[35;1m5.""\033[36;1mCodaShop"
+echo "\033[35;1m6.""\033[36;1mSurveyon"
+echo "\033[35;1m7.""\033[36;1mCustom"
+echo "\033[31;1m0. Keluar"
+echo "\033[37;1m8.""\033[30;1mInformasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
+sleep 5
+clear
+cd $HOME/unlimitedsms/Tul/
+php 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
+sleep 5
+clear
+cd $HOME/unlimitedsms/Tul/
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
+sleep 5
+clear
+cd $HOME/unlimitedsms/Tul/
+php 3.php
+fi
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono9 -F gay "Toked"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
+sleep 5
+clear
+cd $HOME/unlimitedsms/Tul/
+php 4.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant -F gay "CodaShop"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
+sleep 5
+clear
+cd $HOME/unlimitedsms/Tul/
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Surveyon "
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
+sleep 5
+clear
+cd $HOME/unlimitedsms/Tul/
+php 6.php
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant --gay "SMSID"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m100"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
+echo
+echo
+echo "\033[33;1mInformasi Lebih Lanjut Kunjungi:""\033[36;1mhttps://github.com/hryar/"
+sleep 5
+smsid boom -y
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "unlimitedsms"
+echo "\033[34;1mContact:""\033[37;1m https://m.facebook.com/arbi0510"
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+sleel 1
+echo "\033[33;1mKarya: Hry Ar"
+sleep 1
+echo "\033[32;1mVersi: v5.0"
+sleep 1
+echo "\033[36;1mTeam: Gumay Talang"
+sleep 1
+echo "\033[31;1mSUBSCRIBE YOUTUBE""\033[37;1m HARRY ARBI"
+echo "LIKE"
+echo "SHARE"
+echo "COMENTS"
+echo "SUBSCRIBE"
+echo "THANKS YOU"
+echo "\033[30;1m tunggu 5 detik"
+sleep 5
+sh unlimitedsms.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m See You Nets Time :)"
+sleep 1
+fi
