bfa306b917daeb9e138ad45a50af07eed206105a
hryar <74238336+hryar@users.noreply.github.com>
2020-11-10 22:26:45 +0700

    Update unlimitedsms.sh

--- unlimitedsms.sh
+++ unlimitedsms.sh
@@ -8,29 +8,29 @@ clear
 
 toilet -f slant --gay "unlimitedsms"
 sleep 1
-echo "\033[33;1m Macam-macam tool untuk spam"
+echo "\033[33;1mMacam-Macam Tools Spam"
 sleep 1
-echo "\033[36;1m★""\033[34;1mGumay Talang""\033[36;1m★"
+echo "\033[36;1m★""\033[34;1mHry Ar""\033[36;1m★"
 sleep 1
-echo "\033[32;1m Spam Yang Tersedia :"
-echo "\033[35;1m1.""\033[36;1mBukalapak"
-echo "\033[35;1m2.""\033[36;1mTelkomsel"
-echo "\033[35;1m3.""\033[36;1mGrab"
+echo "\033[32;1mSpam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mGoJek"
+echo "\033[35;1m2.""\033[36;1mGrab"
+echo "\033[35;1m3.""\033[36;1mBukalapak"
 echo "\033[35;1m4.""\033[36;1mTokopedia"
-echo "\033[35;1m5.""\033[36;1mCodaShop"
-echo "\033[35;1m6.""\033[36;1mSurveyon"
+echo "\033[35;1m5.""\033[36;1mLazada"
+echo "\033[35;1m6.""\033[36;1mShopee"
 echo "\033[35;1m7.""\033[36;1mCustom"
-echo "\033[31;1m0. Keluar"
+echo "\033[31;1m0.Keluar"
 echo "\033[37;1m8.""\033[30;1mInformasi Tool Ini"
-echo "\033[33;1m Pilih Angka:"
+echo "\033[33;1mPilih Angka:"
 read mrrm
 
 if [ $mrrm = 1 ] || [ $mrrm = 1 ]
 then
 clear
-toilet -f slant "Bukalapak"
+toilet -f slant "GoJek"
 echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
-echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
 echo "\033[31;1mPemograman:""\033[37;1mPHP"
 echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
 sleep 5
@@ -44,9 +44,9 @@ if
 then
 clear
 echo "\033[31;1m"
-toilet "T-Sel"
+toilet "Grab"
 echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
-echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
 echo "\033[31;1mPemograman:""\033[37;1mPHP"
 echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
 sleep 5
@@ -59,9 +59,9 @@ if [ $mrrm = 3 ] || [ $mrrm = 3 ]
 then
 clear
 echo "\033[36;1m"
-figlet "Grab"
+figlet "Bukalapak"
 echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
-echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
 echo "\033[31;1mPemograman:""\033[37;1mPHP"
 echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
 sleep 5
@@ -73,9 +73,9 @@ fi
 if [ $mrrm = 4 ] || [ $mrrm = 4 ]
 then
 clear
-toilet -f mono9 -F gay "Toked"
+toilet -f slant -F gay "Tokopedia"
 echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
-echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
 echo "\033[31;1mPemograman:""\033[37;1mPHP"
 echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
 sleep 5
@@ -87,9 +87,9 @@ fi
 if [ $mrrm = 5 ] || [ $mrrm = 5 ]
 then
 clear
-toilet -f slant -F gay "CodaShop"
+toilet -f slant -F gay "Lazada"
 echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
-echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
 echo "\033[31;1mPemograman:""\033[37;1mPHP"
 echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
 sleep 5
@@ -102,9 +102,9 @@ if
 [ $mrrm = 6 ] || [ $mrrm = 6 ]
 then
 clear
-toilet -f standard -F gay "Surveyon "
+toilet -f slant -F gay "Shopee"
 echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
-echo "\033[31;1mBatas:""\033[37;1m100 Kali"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
 echo "\033[31;1mPemograman:""\033[37;1mPHP"
 echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
 sleep 5
@@ -119,12 +119,12 @@ then
 clear
 toilet -f slant --gay "SMSID"
 echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
-echo "\033[31;1mBatas:""\033[37;1m100"
-echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mBerjalan Langsung Pada DalvikVM"
 echo "\033[31;1mAuthor:""\033[37;1mHry Ar"
 echo
 echo
-echo "\033[33;1mInformasi Lebih Lanjut Kunjungi:""\033[36;1mhttps://github.com/hryar/"
+echo "\033[33;1mInformasi Lebih Lanjut Kunjungi:""\033[36;1mhttps://amsitlab.github.io/smsid-java/"
 sleep 5
 smsid boom -y
 fi
